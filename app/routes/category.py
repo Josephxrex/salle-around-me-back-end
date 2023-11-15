7a5890bfff7d00bbe44d6bdc7493244d6da3e23e
dansa <dansa@DESKTOP-6M8GITO.local>
2023-11-15 17:53:21 -0600

    Eliminar comentarios de category para corregir swagger

--- app/routes/category.py
+++ app/routes/category.py
@@ -9,39 +9,7 @@ from middleware.middleware import jwt_required
 @category_bp.route('/', methods=['GET'])
 @jwt_required
 def get_categories(data):
-    """
-    Obtener todas las categorías
-    ---
-    parameters:
-      - name: data
-        in: body
-        required: true
-        description: Datos necesarios para obtener todas las categorías.
-    responses:
-      200:
-        description: Lista de categorías.
-        schema:
-          type: array
-          items:
-            type: object
-            properties:
-              id:
-                type: integer
-                description: ID de la categoría.
-              name:
-                type: string
-                description: Nombre de la categoría.
-              description:
-                type: string
-                description: Descripción de la categoría.
-      500:
-        description: Error al obtener las categorías.
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              description: Mensaje de error."""
+    
     try:
         # Consulta todas las Categorías donde id_delete es iguala a 0
         categories = Category.query.filter(Category.is_delete == 0).all()
@@ -65,43 +33,7 @@ def get_categories(data):
 @category_bp.route('/', methods=['POST'])
 @jwt_required
 def create_category(data):
-    """
-      Crear una nueva categoría
-      ---
-      parameters:
-        - name: data
-          in: body
-          required: true
-          description: Datos para crear una nueva categoría.
-          schema:
-            type: object
-            properties:
-              id:
-                type: integer
-                description: ID de la categoría.
-              name:
-                type: string
-                description: Nombre de la categoría.
-              description:
-                type: string
-                description: Descripción de la categoría.
-      responses:
-        200:
-          description: Categoría creada exitosamente.
-          schema:
-            type: object
-            properties:
-              message:
-                type: string
-                description: Mensaje de éxito.
-        500:
-          description: Error al crear la categoría.
-          schema:
-            type: object
-            properties:
-              error:
-                type: string
-                description: Mensaje de error."""
+    
     try:
         data = request.get_json()
         
@@ -122,46 +54,7 @@ def create_category(data):
 @category_bp.route('/<int:id>', methods=['GET'])
 @jwt_required
 def get_category(data,id):
-    """
-      Obtener una categoría por su ID
-      ---
-      parameters:
-        - name: id
-          in: path
-          type: integer
-          required: true
-          description: ID de la categoría que se desea obtener.
-      responses:
-        200:
-          description: Información de la categoría.
-          schema:
-            type: object
-            properties:
-              id:
-                type: integer
-                description: ID de la categoría.
-              name:
-                type: string
-                description: Nombre de la categoría.
-              description:
-                type: string
-                description: Descripción de la categoría.
-        404:
-          description: categoría no encontrada.
-          schema:
-            type: object
-            properties:
-              error:
-                type: string
-                description: Mensaje de error.
-        500:
-          description: Error al obtener la categoría.
-          schema:
-            type: object
-            properties:
-              error:
-                type: string
-                description: Mensaje de error."""
+    
     try:
         category = Category.query.get(id)
 
@@ -176,56 +69,7 @@ def get_category(data,id):
 @category_bp.route('/<int:id>', methods=['PUT'])
 @jwt_required
 def update_category(data, id):
-    """
-    Actualizar una categoría por su ID
-    ---
-    parameters:
-      - name: id
-        in: path
-        type: integer
-        required: true
-        description: ID de la categoría que se desea actualizar.
-      - name: data
-        in: body
-        required: true
-        description: Datos para actualizar la categoría.
-        schema:
-          type: object
-            properties:
-              id:
-                type: integer
-                description: ID de la categoría.
-              name:
-                type: string
-                description: Nombre de la categoría.
-              description:
-                type: string
-                description: Descripción de la categoría.    
-      responses:
-      200:
-        description: categoría actualizada exitosamente.
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-              description: Mensaje de éxito.
-      404:
-        description: categoría no encontrada.
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              description: Mensaje de error.
-      500:
-        description: Error al actualizar la categoría.
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              description: Mensaje de error."""
+    
 
     try:
         category = Category.query.get(id)
@@ -247,40 +91,7 @@ def update_category(data, id):
 @category_bp.route('/<int:id>', methods=['DELETE'])
 @jwt_required
 def delete_category(data, id):
-    """
-    Eliminar una categoría por su ID (Borrado lógico)
-    ---
-    parameters:
-      - name: id
-        in: path
-        type: integer
-        required: true
-        description: ID de la categoría que se desea eliminar (marcar como eliminada).
-    responses:
-      200:
-        description: categoría eliminada exitosamente.
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-              description: Mensaje de éxito.
-      404:
-        description: categoría no encontrada.
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              description: Mensaje de error.
-      500:
-        description: Error al eliminar la categoría.
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              description: Mensaje de error."""
+    
     try:
         # Obtener la atracción existente por su ID
         category = Category.query.get(id)
