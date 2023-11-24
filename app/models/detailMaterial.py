9a965f910bb4f01ab7c5433921835f3abba94d28
jml74262 <jml74262@lasallebajio.edu.mx>
2023-11-24 16:32:19 -0600

    refactor detailMaterial

--- app/models/detailMaterial.py
+++ app/models/detailMaterial.py
@@ -3,5 +3,5 @@ from app import db
 class DetailMaterial(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     material_id = db.Column(db.Integer, db.ForeignKey('material.id'))
-    attraction_id = db.Column(db.Integer, db.ForeignKey('attraction.id'))
+    id_attraction = db.Column(db.Integer, db.ForeignKey('attraction.id'))
     is_delete = db.Column(db.Boolean, default=False)
